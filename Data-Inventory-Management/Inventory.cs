3e2168fb138514c2653808bbef5b9e1619ae19cd
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-06-07 01:32:45 +0530

    [sohail] Add: Data management of inventory and calculated total value of inventory

---
+++ Data-Inventory-Management/Inventory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Inventory_Management
+{
+   public class Inventory
+    {
+            public int SerialNo { get; set; }
+            public string name { get; set; }
+            public int weight { get; set; }
+            public float price { get; set; }
+
+    }
+}
