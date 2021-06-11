24ca6221920b3c5c4e345c0aa1cae295aada3402
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-06-11 23:10:23 +0530

    [sohail] refactor: Implementing simple methods

--- Data-Inventory-Management/Inventory.cs
+++ Data-Inventory-Management/Inventory.cs
@@ -1,15 +1,54 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Data_Inventory_Management
 {
-   public class Inventory
+    /// <summary>
+    /// Inventory class with Methods GetData().
+    /// </summary>
+    public class Inventory
     {
-            public int SerialNo { get; set; }
-            public string name { get; set; }
-            public int weight { get; set; }
-            public float price { get; set; }
+        /// <summary>
+        /// GetData() method of Inventory class, 
+        /// getting data from Json file - consisting of data inventories.
+        /// file - DataInventory.json
+        /// </summary>
+        public void GetData()
+        {
+            string filepath = @"C:\Users\Admin\Desktop\BridgeLabs Assignments\Inventory Management\Data-Inventory-Management\Data-Inventory-Management\DataInventory.json";
+            var jsonOutput = File.ReadAllText(filepath);
+            var jObject = JObject.Parse(jsonOutput);
+            var inventoryArray = (JArray)jObject["inventories"];
+            if (jObject != null)
+            {
+                Console.WriteLine("Inventories");
+                foreach (var item in inventoryArray)
+                {
+                    Console.WriteLine($"Name: {item["name"]}");
+                    Console.WriteLine($"Weight: {item["weight"]}kg(s)");
+                    Console.WriteLine($"price: {item["price"]}.Rs/kg");
+                    GetValue(item["weight"], item["price"]);
+                    Console.WriteLine();
+                }
+            }
+        }
+        /// <summary>
+        /// Calculating value of each inventory.
+        /// passing Jtokens of weight and price 
+        /// </summary>
+        /// <param name="weight"></param>
+        /// <param name="price"></param>
+        public void GetValue(JToken weight, JToken price)
+        {
+            int weightConv = weight.ToObject<int>();
+            int priceConv = price.ToObject<int>();
+            int totalValue = weightConv * priceConv;
+            Console.WriteLine($"Value of Inventory : {totalValue}.Rs");
+        }
 
     }
 }
--- Data-Inventory-Management/Program.cs
+++ Data-Inventory-Management/Program.cs
@@ -16,47 +16,8 @@ namespace Data_Inventory_Management
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Inventory rice = new Inventory()
-            {
-                SerialNo = 1,
-                name = "Rice",
-                weight = 25,
-                price = 50
-            };  Inventory pulses = new Inventory()
-            {
-                SerialNo = 2,
-                name = "Pulses",
-                weight = 50,
-                price = 75
-            };  
-            Inventory wheats = new Inventory()
-            {
-                SerialNo = 3,
-                name = "Wheat",
-                weight = 100,
-                price = 25
-            };
-
-            List<Inventory> inventories = new List<Inventory>();
-            inventories.Add(rice);
-            inventories.Add(pulses);
-            inventories.Add(wheats);
-            
-            //Serialization object from string to json format.//
-            string strResult = JsonConvert.SerializeObject(inventories); 
-            File.WriteAllText(@"Inventort.json", strResult); 
-            Console.WriteLine("Stored successfully!!"); 
-
-            //Deserialization object from json file to string format.//
-            strResult = File.ReadAllText(@"Inventort.json");
-            var list = JsonConvert.DeserializeObject<List<Inventory>>(strResult);
-            float totalValue = 0;
-            foreach (Inventory inventory in list)
-            {
-                Console.WriteLine($"Serial Number: {inventory.SerialNo}\n Name: {inventory.name}\n Weight: {inventory.weight}\n Price: {inventory.price}/Kg\n Total Price: {inventory.weight * inventory.price}\n");
-                totalValue += (inventory.price * inventory.weight);
-            }
-            Console.WriteLine($"Total Value of inventory:{totalValue}");
+            Inventory inventory = new Inventory();
+            inventory.GetData();
 
         }
     }
